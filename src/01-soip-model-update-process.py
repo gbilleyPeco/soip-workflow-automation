59c0ff8e4177aa510949ee06c81dc35214990770
gbilleyPeco <119866794+gbilleyPeco@users.noreply.github.com>
2023-07-14 13:14:27 -0400

    lots o changes

---
+++ src/01-soip-model-update-process.py
@@ -0,0 +1,25 @@
+'''
+The purpose of this script is to replace the set of "SOIP Model Update Process" Alteryx workflows.
+
+Specifically:
+    'SOIP Model Update Process - 000 - Main.yxwz',
+    'SOIP Model Update Process - 010 - Depot Costs and Attributes v2.yxmc',
+    'SOIP Model Update Process - 015 - Set SOIP Solve Flag.yxmd',
+    'SOIP Model Update Process - 017 - Issue and Return Location Details.yxmd',
+    'SOIP Model Update Process - 020 - Lane Attributes.yxmd',
+    'SOIP Model Update Process - 030 - NPD Percentage Penalty.yxmc',
+    'SOIP Model Update Process - 060 - Transportation Rates Historical.yxmc',
+    'SOIP Model Update Process - 070 - Trans Load Size.yxmc',
+    'SOIP Model Update Process - 090 - Flag Multi-Source Options.yxmc',
+    'SOIP Model Update Process - 100 - Transfer Matrix Update.yxmc',
+    'SOIP Model Update Process - 110 - Renter Dist Sort Pref Depot.yxmc',
+'''
+
+#%% ################################################################################################
+
+
+
+
+
+
+#%% ################################################################################################
