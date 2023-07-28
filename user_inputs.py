0d832b2322ab64f660c584e29f6b52457745d7c6
gbilleyPeco <119866794+gbilleyPeco@users.noreply.github.com>
2023-07-28 14:43:00 -0400

    Update user_inputs.py

--- user_inputs.py
+++ user_inputs.py
@@ -9,13 +9,14 @@ APP_KEY = 'op_NWQ3YjQ0NjktNTBjOC00M2JkLWE4NWEtNjM1NDBmODA5ODEw'
 DB_NAME = 'PECO 2023-08 SOIP Opt copy 1' # Opt Model Name
 
 # SOIP Excel File Information
-SOIP_DEPOT_ASSIGNMENTS_FILENAME = 'SOIP - Depot Assignments - July.xlsx'
-SOIP_OPT_ASSUMPTIONS_FILENAME = 'SOIP Optimization Assumptions - 20230620.xlsx'
+SOIP_DEPOT_ASSIGNMENTS_FILENAME = 'SOIP - Depot Assignments - TESTING.xlsx'
+SOIP_OPT_ASSUMPTIONS_FILENAME = 'SOIP Optimization Assumptions - TESTING.xlsx'
 
 # User supplied notes in Cosmic Frog
 RepairCapacityNotes = 'Repair_Capacity'
 MinInventoryNotes = "MinInventory"
 DepotCapacityNotes = "Depot_Capacity"
 BeginningInvNotes = 'SOIP_BeginningInventory'
-ReturnsProductionNotes = 'Returns_SeptemberSOIP'
+ReturnsProductionNotes = 'Returns_SOIP'
 CustomerDemandNotes = 'Issues_SeptemberSOIP'
+ProductionPolicyRepairBOMName = 'BOM_RFU_REPAIR'
