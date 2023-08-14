b02ded2318192c816a87245617e39eeee93b3c93
gbilleyPeco <119866794+gbilleyPeco@users.noreply.github.com>
2023-08-14 12:54:44 -0400

    Update RUN MODEL.bat

--- RUN MODEL.bat
+++ RUN MODEL.bat
@@ -2,6 +2,7 @@ call C:\Users\gbilley\Anaconda3\condabin\activate.bat
 ::call conda env create -f environment.yml
 ::call conda env update -f environment.yml
 call conda activate soip-workflow-automation
-python %~dp0src\01-soip-model-update-process.py
+python %~dp0src\soip_model_update_process.py
+python %~dp0src\soip_model_update_validation.py
 call conda deactivate
 pause
