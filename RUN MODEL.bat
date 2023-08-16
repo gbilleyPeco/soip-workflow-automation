00022202f49865d49a1872a893a96474a023bc30
gbilleyPeco <119866794+gbilleyPeco@users.noreply.github.com>
2023-08-16 10:03:30 -0400

    Update RUN MODEL.bat

--- RUN MODEL.bat
+++ RUN MODEL.bat
@@ -1,6 +1,6 @@
 call C:\Users\gbilley\Anaconda3\condabin\activate.bat
-::call conda env create -f environment.yml
-::call conda env update -f environment.yml
+call conda env create -f environment.yml
+call conda env update -f environment.yml
 call conda activate soip-workflow-automation
 python %~dp0src\soip_model_update_process.py
 python %~dp0src\soip_model_update_validation.py
